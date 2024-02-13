b5ff05c89f14a8558209c91aa4e33ccf19405b87
vmahajan <v_mahajan@yahoo.com>
2024-02-13 02:47:06 -0400

    updated Item page

--- eCommerce/my-ecom-site/src/Components/ProductDisplay/ProductDisplay.jsx
+++ eCommerce/my-ecom-site/src/Components/ProductDisplay/ProductDisplay.jsx
@@ -9,13 +9,13 @@ export const ProductDisplay = (props) => {
     <div className='productdisplay'>
         <div className="productdisplay-left">
             <div className="productdisplay-img-list">
-                <img src={product.image} alt="" />
-                <img src={product.image} alt="" />
-                <img src={product.image} alt="" />
-                <img src={product.image} alt="" />
+                <img src={product.image} alt="" width='100px' height='80px' />
+                <img src={product.image} alt=""  width='100px' height='80px'/>
+                <img src={product.image} alt=""  width='100px' height='80px'/>
+                <img src={product.image} alt=""  width='100px' height='80px'/>
             </div>
             <div className="productdisplay-img">
-                <img src={product.image} alt="" className="productdisplay-main-img" />
+                <img src={product.image} alt="" className="productdisplay-main-img" height='500px' width='500px'/>
             </div>
         </div>
         <div className="productdisplay-right">
@@ -26,7 +26,29 @@ export const ProductDisplay = (props) => {
                 <img src={star_icon} alt="" width='20px' height='20px'/>
                 <img src={star_icon} alt="" width='20px' height='20px'/>
                 <img src={star_dull_icon} alt="" width='20px' height='20px'/>
+                <p>(122)</p>
             </div>
+            <div className="productdisplay-right-prices">
+                <div className="productdisplay-right-price-old">${product.old_price}</div>
+                <div className="productdisplay-right-price-new">${product.new_price}</div>
+            </div>
+            <div className="productdisplay-right-description">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos quasi nihil 
+                doloribus modi numquam, repellat sit maiores sapiente tenetur accusamus similique veritatis voluptatum ut!
+            </div>
+            <div className="productdisplay-right-size">
+                <h1>Select Size</h1>
+                <div className="productdisplay-right-sizes">
+                    <div>S</div>
+                    <div>M</div>
+                    <div>L</div>
+                    <div>XL</div>
+                    <div>XXL</div>
+                </div>
+            </div>
+            <button>ADD TO CART</button>
+            <p className="productdisplay-right-category"><span>Category: </span>women, T-shirt, Crop Top</p>
+            <p className="productdisplay-right-category"><span>Tags: </span>Modern, Latest</p>
 
         </div>
     </div>
